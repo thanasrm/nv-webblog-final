c0c8358d7d935923b876a09b644a31bd0c7d1e03
thanasrm <111999606+thanasrm@users.noreply.github.com>
2022-10-07 02:06:51 +0700

    jj

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,7 +2,7 @@
 	<div>
 		<div class="nv-navbar">
 			<ul class="nav">
-				<li><router-link :to="{ name: 'blogs' }">JobJob</router-link></li>
+				<li><router-link :to="{ name: 'blogs' }">thana</router-link></li>
 				<li><router-link :to="{ name: 'users' }">Users</router-link></li>
 				<li><router-link :to="{ name: 'comments' }">Comments</router-link></li>
 				<li><router-link :to="{ name: 'login' }">Login</router-link></li>
